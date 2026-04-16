a1a3ced03f3d73b253223a9a1dcf56b162d7dec3
hikennii <kennyto08618@gmail.com>
2026-04-15 21:41:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import './index.css'
+import "./index.css";
 import Menu from "./components/menu.jsx";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 import projectImg from "./assets/project.png";
 import aboutImg from "./assets/about.png";
@@ -17,48 +17,53 @@ function App() {
     About: aboutImg,
     Experience: experienceImg,
     Resume: resumeImg,
-    Contact: contactImg
+    Contact: contactImg,
   };
 
   return (
     <>
       <video
-      autoPlay
-      loop
-      muted
-      playsInline
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        objectFit: "cover",
-        zIndex: -1,
-      }}
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: -1,
+        }}
       >
         <source src="/background.mp4" type="video/mp4" />
       </video>
 
       <div style={{ display: "flex", height: "100vh", color: "white" }}>
+        
         <div className="left">
           <Menu active={active} setActive={setActive} />
         </div>
 
         <div className="right">
-          <motion.img
-            key={active}
-            src={content[active]}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.4}}
-            style={{
-              width: "25%",
-              maxWidth: "700px",
-              borderRadius: "10px"
-            }}
-          />
+          <AnimatePresence mode="wait">
+            <motion.img
+              key={active}
+              src={content[active]}
+              initial={{ opacity: 0, scale: 0.98 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 1.02 }}
+              transition={{ duration: 0.29}}
+              style={{
+                width: "25%",
+                maxWidth: "700px",
+                borderRadius: "10px",
+              }}
+            />
+          </AnimatePresence>
         </div>
+
       </div>
     </>
   );
