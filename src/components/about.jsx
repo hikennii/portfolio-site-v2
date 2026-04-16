15d0ba9cc3d5a9ded4a958344e2a13e84591ef02
hikennii <kennyto08618@gmail.com>
2026-04-16 17:28:31 -0400

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,7 +12,7 @@ export default function About() {
         <br/><br/>
         On the technical side, I’ve worked as a Level 1 Consultant with Rutgers Office of Information Technology, where I help troubleshoot issues and support students, faculty, and staff. I also like working on projects where I can build things that are both functional and easy to use.
         <br/><br/>
-        At the same time, I have a strong background in media. I’ve done graphic design, photo editing, and content creation for social media, and I’ve worked on larger creative projects like videos and event productions. That experience changed how I approach my work. I pay attention not just to how something works, but how it looks and feels.
+        At the same time, I have a strong background in media. I’ve done graphic design, photo editing, and content creation for social media, and I’ve worked on larger creative projects like videos and event productions. I pay attention not just to how something works, but how it looks and feels.
         <br/><br/>
         Overall, I enjoy combining problem solving with creativity. Whether I’m working on a technical project or something media related, I try to make it clean, engaging, and well thought out. I’m looking to keep growing in roles where I can use both my technical skills and creative side.
         
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,19 @@
+import "../index.css";
+import linkedinImg from "../assets/linkedin.png";
+import githubImg from "../assets/github.png";
+
 export default function Contact() {
   return (
     <div className="info-container">
-      <div>My Contact Info</div>
+      <h2>Email: <strong>kennyto08618@gmail.com</strong></h2>
+      <div style={{display:"flex", justifyContent:"center"}}>
+        <a href="https://www.linkedin.com/in/kenny-le-3b81bb320/" target="_blank" style={{display:"flex", alignItems:"center"}}>
+          <img src={linkedinImg} style={{width:"50px", display:"block"}}></img>
+        </a>
+        <a href="https://github.com/hikennii" target="_blank" style={{display:"flex", alignItems:"center"}}>
+          <img src={githubImg} style={{width:"50px", display:"block"}}></img>
+        </a>
+      </div>
     </div>
 )
 }
