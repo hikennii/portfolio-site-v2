c4c553a9f3d865804926778ff6ab75101d5c8e2a
hikennii <kennyto08618@gmail.com>
2026-04-17 01:43:09 -0400

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,16 +7,18 @@ export default function About() {
       <h1>About me!</h1>
       <img src={meImg} style={{width:"10%", margin:"0"}}/>
       <h2>I'm Kenny!</h2>
-      <h3>
-        I’m an Information Technology and Informatics student at Rutgers University with minors in Data Science <br/>and Digital Communication, Information, and Media. I’m interested in the mix between technology, data, and creative media.
-        <br/><br/>
-        On the technical side, I’ve worked as a Level 1 Consultant with Rutgers Office of Information Technology, where I help troubleshoot <br/>issues and support students, faculty, and staff. I also like working on projects where I can build things that are both functional and easy to use.
-        <br/><br/>
-        At the same time, I have a strong background in media. I’ve done graphic design, photo editing, and content creation for social media, <br/>and I’ve worked on larger creative projects like videos and event productions. I pay attention not just to how something works, but how it looks and feels.
-        <br/><br/>
-        Overall, I enjoy combining problem solving with creativity. Whether I’m working on a technical project or something media related, <br/>I try to make it clean, engaging, and well thought out. I’m looking to keep growing in roles where I can use both my technical skills and creative side.
-        
-      </h3>
+      <div style={{width:"80%"}}>
+        <h3>
+          I’m an Information Technology and Informatics student at Rutgers University with minors in Data Science <br/>and Digital Communication, Information, and Media. I’m interested in the mix between technology, data, and creative media.
+          <br/><br/>
+          On the technical side, I’ve worked as a Level 1 Consultant with Rutgers Office of Information Technology, where I help troubleshoot <br/>issues and support students, faculty, and staff. I also like working on projects where I can build things that are both functional and easy to use.
+          <br/><br/>
+          At the same time, I have a strong background in media. I’ve done graphic design, photo editing, and content creation for social media, <br/>and I’ve worked on larger creative projects like videos and event productions. I pay attention not just to how something works, but how it looks and feels.
+          <br/><br/>
+          Overall, I enjoy combining problem solving with creativity. Whether I’m working on a technical project or something media related, <br/>I try to make it clean, engaging, and well thought out. I’m looking to keep growing in roles where I can use both my technical skills and creative side.
+          
+        </h3>
+      </div>
     </div>
   )
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,6 +5,8 @@ import githubImg from "../assets/github.png";
 export default function Contact() {
   return (
     <div className="info-container">
+      <h1>Contact</h1>
+      <br/>
       <h2>Email: <strong>kennyto08618@gmail.com</strong></h2>
       <div style={{display:"flex", justifyContent:"center"}}>
         <a href="https://www.linkedin.com/in/kenny-le-3b81bb320/" target="_blank" style={{display:"flex", alignItems:"center"}}>
